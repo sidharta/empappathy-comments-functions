eedd1dbb2640ac269ada1e89f2d08cddcc82557c
Sidharta Noleto <sidvelloso@gmail.com>
2017-03-07 00:14:06 -0300

    changing the way we save sentiment score on database

--- function.js
+++ function.js
@@ -17,30 +17,45 @@ firebase.initializeApp({
 var language = gcloud.language()
 
 function commentAnalysis(req, res) {
-  var postKey = req.body.postKey
+
+  // Set CORS headers
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if ( req.method === 'OPTIONS' ) {
+    console.log('Preflight request...')
+    res.writeHead(200);
+		res.end();
+		return;
+	}
+
   var commentKey = req.body.commentKey
 
-  if( !postKey && !commentKey ) {
-    console.log("No postKey and commentKey provided!")
-    return
-  }
+  if( !commentKey ) {
+    console.log("No commentKey provided!")
+  } else {
+    var commentRef = firebase.database()
+      .ref("/user-comments/" + commentKey)
 
-  var commentRef = firebase.database()
-    .ref("/user-comments/" + postKey)
-    .child(commentKey)
+    commentRef.once("value", function(snap) {
+      var comment = snap.val()
 
-  commentRef.once("value", function(snap) {
-    var comment = snap.val()
+      language.detectSentiment(comment.comment, function(err, sentiment){
+        if(err) {
+          console.log(err)
+        } else {
+          var score = {
+            score: sentiment,
+            postKey: comment.postKey
+          }
 
-    language.detectSentiment(comment.comment, function(err, sentiment){
-      if(err) {
-        console.log(err)
-      } else {
-        comment.score = sentiment
-        commentRef.update(comment)
-      }
+          firebase.database().ref('/comment-score/' + commentKey).update(score)
+        }
+      })
     })
-  })
+  }
 
   res.send('OK')
 }
